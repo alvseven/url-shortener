b34adcada94b6f23c05fa76b056444d5171fbf86
alvseven <alv7contato@outlook.com>
2024-11-02 03:41:47 -0300

    refactor: delete migration create users table

--- prisma/migrations/20241102064104_/migration.sql
+++ prisma/migrations/20241102064104_/migration.sql
@@ -1,18 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
 -- CreateTable
 CREATE TABLE "links" (
     "id" TEXT NOT NULL,
     "originalUrl" TEXT NOT NULL,
     "shortUrl" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "clicks" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "links_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "links_shortUrl_key" ON "links"("shortUrl");
-
--- AddForeignKey
-ALTER TABLE "links" ADD CONSTRAINT "links_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
