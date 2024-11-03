0dd4896463c268f6bc41efec25c6b56bbb424fa2
alvseven <alv7contato@outlook.com>
2024-11-02 22:59:51 -0300

    refactor: remove module file, fix import and url link

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { LinkModule } from './modules/links/link.module';
+import { LinkModule } from './modules/links/links.module';
 
 @Module({
   imports: [LinkModule],
