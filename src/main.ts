2a20513b52bec49591081f64f6cfa3fcb3685904
alvseven <alv7contato@outlook.com>
2024-11-03 20:59:29 -0300

    docs: basic endpoints doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { parsedEnvs } from './shared/config/env';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription(
+      'An api to shorten URLs with user authentication and clicks count',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(parsedEnvs.API_PORT);
 }
